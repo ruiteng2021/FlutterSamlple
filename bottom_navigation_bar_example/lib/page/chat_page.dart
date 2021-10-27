8a534fd983e4bfe1a6ee1412205687b097b5312f
tengrui <tengrui13@gmail.com>
2021-10-26 20:32:59 -0400

    bottom_navigation_bar_example

---
+++ bottom_navigation_bar_example/lib/page/chat_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat'),
+      ),
+      body: Center(child: Text('Chat', style: TextStyle(fontSize: 60))),
+    );
+  }
+}
---
+++ bottom_navigation_bar_example/lib/page/feed_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class FeedPage extends StatefulWidget {
+  @override
+  _FeedPageState createState() => _FeedPageState();
+}
+
+class _FeedPageState extends State<FeedPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Feed'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ), // This t
+      );
+}

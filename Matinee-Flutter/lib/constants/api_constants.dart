b1bce4fd9a7706cfc4e5bbe65d20401863c2faf0
tengrui13@gmail.com <tengrui13@gmail.com>
2021-12-20 17:47:04 -0500

    Search text bar

--- Matinee-Flutter/lib/constants/api_constants.dart
+++ Matinee-Flutter/lib/constants/api_constants.dart
@@ -1,3 +1,3 @@
 const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
-const String TMDB_API_KEY = "YOUR_API_KEY";
+const String TMDB_API_KEY = "d194eb72915bc79fac2eb1a70a71ddd3";
 const String TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
